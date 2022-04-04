9f30335d178a7cc543ed9c7745927fe9377bfa2b
Lewis Rosario <lewisrosario94@gmail.com>
2022-04-04 12:04:26 -0400

    Primer commit

---
+++ bbdd.js
@@ -0,0 +1,272 @@
+export const USER_BBDD = [
+    {
+      "_id": "62471918a7e4cef7222b01be",
+      "index": 0,
+      "guid": "ded50a13-7c92-4a0d-a2eb-486e1fbdaab1",
+      "isActive": true,
+      "balance": "$3,373.25",
+      "picture": "http://placehold.it/32x32",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": "Jessie Booth",
+      "gender": "female",
+      "company": "INFOTRIPS",
+      "email": "jessiebooth@infotrips.com",
+      "phone": "+1 (977) 489-2625",
+      "address": "855 Clarendon Road, Colton, Missouri, 6764",
+      "about": "Officia minim qui irure et aliqua ad qui minim. Tempor elit mollit id magna do aute fugiat culpa. Sunt sunt elit officia cupidatat laborum est id irure.\r\n",
+      "registered": "2014-08-20T04:14:23 +04:00",
+      "latitude": -34.363201,
+      "longitude": -41.470547,
+      "tags": [
+        "esse",
+        "ipsum",
+        "labore",
+        "excepteur",
+        "Lorem",
+        "ad",
+        "anim"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Burke Wong"
+        },
+        {
+          "id": 1,
+          "name": "Sharp Hardin"
+        },
+        {
+          "id": 2,
+          "name": "Lessie Barron"
+        }
+      ],
+      "greeting": "Hello, Jessie Booth! You have 10 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "62471918f727079f871fda37",
+      "index": 1,
+      "guid": "5c1441aa-84d2-41b8-afbf-0b92e4df2d25",
+      "isActive": false,
+      "balance": "$3470.49",
+      "picture": "http://placehold.it/32x32",
+      "age": 28,
+      "eyeColor": "blue",
+      "name": "Joyce Riddle",
+      "gender": "female",
+      "company": "LIQUIDOC",
+      "email": "joyceriddle@liquidoc.com",
+      "phone": "+1 (861) 548-3148",
+      "address": "794 Wilson Street, Drytown, Arkansas, 7681",
+      "about": "Nisi excepteur nisi labore esse duis aliqua aliqua velit ex. Aliquip non ex non sunt aliquip ex laborum aute consectetur proident culpa. Ex labore mollit amet tempor est ex duis. Quis proident veniam aliqua minim non duis. Enim labore est minim magna velit eiusmod ipsum aute. Consectetur consectetur elit duis fugiat id consequat aliquip consectetur. Quis eu velit occaecat quis consectetur.\r\n",
+      "registered": "2021-03-25T05:05:54 +04:00",
+      "latitude": -29.372001,
+      "longitude": -75.56637,
+      "tags": [
+        "mollit",
+        "aliqua",
+        "reprehenderit",
+        "nisi",
+        "id",
+        "minim",
+        "est"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lauri Hubbard"
+        },
+        {
+          "id": 1,
+          "name": "Jenkins Robinson"
+        },
+        {
+          "id": 2,
+          "name": "Terrie Fuentes"
+        }
+      ],
+      "greeting": "Hello, Joyce Riddle! You have 8 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "624719187931092aad5316c3",
+      "index": 2,
+      "guid": "ab6b57e9-78fb-4035-bab3-8904ab4e5a3a",
+      "isActive": false,
+      "balance": "$2,684.29",
+      "picture": "http://placehold.it/32x32",
+      "age": 34,
+      "eyeColor": "brown",
+      "name": "Felicia English",
+      "gender": "female",
+      "company": "PANZENT",
+      "email": "feliciaenglish@panzent.com",
+      "phone": "+1 (996) 490-2699",
+      "address": "136 Atlantic Avenue, Succasunna, Northern Mariana Islands, 9127",
+      "about": "Cupidatat laborum ullamco commodo et consequat. Est commodo dolore enim enim laborum sunt sit eiusmod id. Nulla adipisicing consequat ullamco cillum excepteur nisi excepteur. Fugiat dolor irure ea exercitation minim aliquip Lorem cupidatat magna dolore anim. Duis est quis veniam minim irure. Elit voluptate consequat quis labore ex. Ex magna aute deserunt officia cupidatat.\r\n",
+      "registered": "2021-09-20T12:09:19 +04:00",
+      "latitude": 55.462119,
+      "longitude": -40.401053,
+      "tags": [
+        "duis",
+        "cupidatat",
+        "nisi",
+        "esse",
+        "dolore",
+        "do",
+        "reprehenderit"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Bryan Morse"
+        },
+        {
+          "id": 1,
+          "name": "Warren Doyle"
+        },
+        {
+          "id": 2,
+          "name": "Logan Cross"
+        }
+      ],
+      "greeting": "Hello, Felicia English! You have 1 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "62471918ad8e2415251318f8",
+      "index": 3,
+      "guid": "5e5f487a-b77d-408b-aa14-8c337f1db1d1",
+      "isActive": true,
+      "balance": "$2,262.54",
+      "picture": "http://placehold.it/32x32",
+      "age": 36,
+      "eyeColor": "green",
+      "name": "Tanya Myers",
+      "gender": "female",
+      "company": "GOLOGY",
+      "email": "tanyamyers@gology.com",
+      "phone": "+1 (856) 489-2917",
+      "address": "796 Vermont Street, Oneida, Pennsylvania, 8849",
+      "about": "Pariatur qui nisi dolore aute. Adipisicing fugiat laboris incididunt do in. Officia nisi officia consectetur magna laboris tempor. Fugiat irure excepteur labore id consectetur adipisicing sunt cupidatat qui esse reprehenderit Lorem.\r\n",
+      "registered": "2021-01-15T05:48:32 +04:00",
+      "latitude": -10.544171,
+      "longitude": -1.00766,
+      "tags": [
+        "dolore",
+        "consequat",
+        "reprehenderit",
+        "laboris",
+        "nisi",
+        "voluptate",
+        "ea"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Ford Barry"
+        },
+        {
+          "id": 1,
+          "name": "Malone Reyes"
+        },
+        {
+          "id": 2,
+          "name": "Madelyn Levine"
+        }
+      ],
+      "greeting": "Hello, Tanya Myers! You have 7 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "62471918eec25edf3dbdfdf4",
+      "index": 4,
+      "guid": "553d8e41-f33d-439a-b79f-eb990ac735df",
+      "isActive": false,
+      "balance": "$3,245.80",
+      "picture": "http://placehold.it/32x32",
+      "age": 35,
+      "eyeColor": "blue",
+      "name": "Deborah Coleman",
+      "gender": "female",
+      "company": "SOLGAN",
+      "email": "deborahcoleman@solgan.com",
+      "phone": "+1 (983) 435-3229",
+      "address": "669 Elm Place, Rehrersburg, Vermont, 5981",
+      "about": "In ea Lorem culpa aliqua ipsum dolore ut. Aliqua dolor consectetur mollit officia eiusmod ipsum ullamco. Quis magna elit nostrud deserunt laboris aute deserunt elit adipisicing et deserunt. Sunt eiusmod exercitation ad et laborum sit mollit velit aliqua sint ea duis. Ullamco eiusmod aute ea consequat qui in veniam ipsum. Mollit aute et id do mollit reprehenderit deserunt adipisicing culpa minim enim quis laboris.\r\n",
+      "registered": "2017-02-10T09:20:41 +04:00",
+      "latitude": -85.081002,
+      "longitude": 79.135331,
+      "tags": [
+        "esse",
+        "amet",
+        "ad",
+        "nisi",
+        "velit",
+        "officia",
+        "sint"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Marquez Lowery"
+        },
+        {
+          "id": 1,
+          "name": "Francine Sherman"
+        },
+        {
+          "id": 2,
+          "name": "Allison Chen"
+        }
+      ],
+      "greeting": "Hello, Deborah Coleman! You have 8 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "62471918ea4cc82f091648d3",
+      "index": 5,
+      "guid": "b620eb1c-a2a4-4be9-be1b-e6cd050e8325",
+      "isActive": true,
+      "balance": "$1,075.40",
+      "picture": "http://placehold.it/32x32",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Lana Bird",
+      "gender": "female",
+      "company": "XTH",
+      "email": "lanabird@xth.com",
+      "phone": "+1 (994) 416-3704",
+      "address": "410 Olive Street, Driftwood, Maryland, 3699",
+      "about": "Cupidatat duis dolor est laborum. Ullamco tempor eu in magna. Eiusmod id et laboris id magna non.\r\n",
+      "registered": "2016-02-18T09:44:26 +04:00",
+      "latitude": 17.424905,
+      "longitude": 150.087327,
+      "tags": [
+        "aliquip",
+        "esse",
+        "exercitation",
+        "amet",
+        "dolore",
+        "labore",
+        "reprehenderit"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Janell Serrano"
+        },
+        {
+          "id": 1,
+          "name": "Bullock Hayden"
+        },
+        {
+          "id": 2,
+          "name": "Colette Aguilar"
+        }
+      ],
+      "greeting": "Hello, Lana Bird! You have 1 unread messages.",
+      "favoriteFruit": "apple"
+    }
+  ]
